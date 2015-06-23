59a4a790a024f9e5f0654c655df9973449175556
olegfox <info@olegfox.com>
2015-06-23 15:59:16 +0300

    new fixes

--- src/Site/MainBundle/Resources/public/frontend/scripts/main.js
+++ src/Site/MainBundle/Resources/public/frontend/scripts/main.js
@@ -1,6 +1,6 @@
 $(function () {
-  // Video
-  $('.block2 .video').vide('/bundles/sitemain/frontend/video/video.mp4', {
+  // Video Main
+  $('.background-video .video').vide('/bundles/sitemain/frontend/video/video2.mp4', {
     muted: true,
     loop: true,
     autoplay: true,
@@ -8,12 +8,25 @@ $(function () {
     resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
   });
 
+  // Video
+  //$('.block2 .video').vide('video/video.mp4', {
+  //  muted: true,
+  //  loop: true,
+  //  autoplay: true,
+  //  position: '50% 50%', // Similar to the CSS `background-position` property.
+  //  resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
+  //});
+
   // Review Slider
   $('.reviews').slick({
     dots: false,
     infinite: true,
     speed: 500,
-    fade: false
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
   });
 
   // Sticky Menu
