cccd4955046add36b411f6504b91ac5ea4b6f6e4
olegfox <info@olegfox.com>
2015-06-26 23:21:46 +0300

    redisign bassejny

--- src/Site/MainBundle/Resources/public/frontend/scripts/main.js
+++ src/Site/MainBundle/Resources/public/frontend/scripts/main.js
@@ -1,5 +1,14 @@
 $(function () {
   // Video Main
+  $('.wrap-video .video').vide('/bundles/sitemain/frontend/video/video2.mp4', {
+    muted: true,
+    loop: true,
+    autoplay: true,
+    position: '50% 50%', // Similar to the CSS `background-position` property.
+    resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
+  });
+
+  // Video Main
   $('.background-video .video').vide('/bundles/sitemain/frontend/video/video2.mp4', {
     muted: true,
     loop: true,
