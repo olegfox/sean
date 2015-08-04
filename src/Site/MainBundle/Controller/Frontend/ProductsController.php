ff71053079cdf7fb6ce2998f2cde6bc84a1696d0
olegfox <info@olegfox.com>
2015-08-04 18:43:23 +0300

    after remove scrolling

--- src/Site/MainBundle/Controller/Frontend/ProductsController.php
+++ src/Site/MainBundle/Controller/Frontend/ProductsController.php
@@ -12,11 +12,9 @@ class ProductsController extends Controller
 
         $product = $repository_products->findOneByPermalink($permalink);
         $products = $repository_products->findAllWithoutParent();
-        $products2 = $repository_products->findAll();
 
         $params = array(
             'products' => $products,
-            'products2' => $products2,
             'product' => $product
         );
 
--- src/Site/MainBundle/Resources/public/frontend/scripts/scroll.js
+++ src/Site/MainBundle/Resources/public/frontend/scripts/scroll.js
@@ -1,66 +1,49 @@
 $(function(){
-  $(".content").each(function(i, e){
-    if(i == 0){
-      $(e).css({
+    $(".content").css({
         'min-height' : $(window).height() - $('.header-top').height()
-      });
-    }else{
-      $(e).css({
-        'min-height' : $(window).height()
-      });
-    }
-  });
+    });
 
-  $(".content .wrap_text").each(function(i, e){
-    if(i == 0){
-      $(e).css({
-        'min-height' : $(e).parent().height() - $(e).parent().find('.wrap_head').height() - $(e).parent().find('.footer').height()
-      });
-    }else{
-      $(e).css({
-        'min-height' : $(e).parent().height() - $(e).parent().find('.wrap_head').height() - $(e).parent().find('.footer').height()
-      });
-    }
-  });
-
+    $(".content .wrap_text").css({
+        'min-height' : $(".content .wrap_text").parent().height() - $(".content .wrap_text").parent().find('.wrap_head').height() - $(".content .wrap_text").parent().find('.footer').height()
+    });
   /* ---------------------------------------------- /*
    * Scroll Animation
    /* ---------------------------------------------- */
 
-  $('.menu-left li a').bind('click', function (e) {
-    var anchor = $(this);
-    $prev = $(".content:visible");
-
-    $("*[id='"+anchor.attr('href')+"']").show();
-
-    if($prev.index() > $("*[id='"+anchor.attr('href')+"']").index()){
-      $('.wrap-container-box').scrollTop($(window).scrollTop() + $("*[id='"+anchor.attr('href')+"']").height());
-    }
-    if (!anchor.parent().hasClass('last')) {
-      $('.menu-left li').removeClass('current');
-      anchor.parent().addClass('current');
-      if(anchor.attr('href') == $('.menu-left > ul > li:first > a').attr('href')){
-        $('.wrap-container-box').stop().animate({
-          scrollTop: 0
-        }, 1000, function(){
-          $prev.hide();
-        });
-      }else{
-        $('.wrap-container-box').stop().animate({
-          scrollTop: $("*[id='"+anchor.attr('href')+"']").offset().top
-        }, 1000, function(){
-          $prev.hide();
-          if($('.wrap-container-box').scrollTop() > 181){
-            $('.wrap-container-box').scrollTop(181);
-          }
-        });
-      }
-      if (window.history.pushState) {
-        window.history.pushState(null, null, anchor.attr('href'));
-      }
-      e.preventDefault();
-    }
-  });
+  //$('.menu-left li a').bind('click', function (e) {
+  //  var anchor = $(this);
+  //  $prev = $(".content:visible");
+  //
+  //  $("*[id='"+anchor.attr('href')+"']").show();
+  //
+  //  if($prev.index() > $("*[id='"+anchor.attr('href')+"']").index()){
+  //    $('.wrap-container-box').scrollTop($(window).scrollTop() + $("*[id='"+anchor.attr('href')+"']").height());
+  //  }
+  //  if (!anchor.parent().hasClass('last')) {
+  //    $('.menu-left li').removeClass('current');
+  //    anchor.parent().addClass('current');
+  //    if(anchor.attr('href') == $('.menu-left > ul > li:first > a').attr('href')){
+  //      $('.wrap-container-box').stop().animate({
+  //        scrollTop: 0
+  //      }, 1000, function(){
+  //        $prev.hide();
+  //      });
+  //    }else{
+  //      $('.wrap-container-box').stop().animate({
+  //        scrollTop: $("*[id='"+anchor.attr('href')+"']").offset().top
+  //      }, 1000, function(){
+  //        $prev.hide();
+  //        if($('.wrap-container-box').scrollTop() > 181){
+  //          $('.wrap-container-box').scrollTop(181);
+  //        }
+  //      });
+  //    }
+  //    if (window.history.pushState) {
+  //      window.history.pushState(null, null, anchor.attr('href'));
+  //    }
+  //    e.preventDefault();
+  //  }
+  //});
 
   //$(window).scroll(function(){
   //  $('.content').each(function(i, e){
