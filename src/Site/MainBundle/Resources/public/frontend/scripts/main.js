ba66d6eaa468f06d22fdc3a8bb40c0b9d751b1c2
olegfox <info@olegfox.com>
2015-09-12 22:17:52 +0300

    add line-video

--- src/Site/MainBundle/Resources/public/frontend/scripts/main.js
+++ src/Site/MainBundle/Resources/public/frontend/scripts/main.js
@@ -17,14 +17,14 @@ $(function () {
   //  resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
   //});
   //
-  ////Video
-  //$('.block2 .video').vide('video/video.mp4', {
-  //  muted: true,
-  //  loop: true,
-  //  autoplay: true,
-  //  position: '50% 50%', // Similar to the CSS `background-position` property.
-  //  resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
-  //});
+  //Video
+  $('.line-video-border').vide('/bundles/sitemain/frontend/video/video2.mp4', {
+    muted: true,
+    loop: true,
+    autoplay: true,
+    position: '50% 50%', // Similar to the CSS `background-position` property.
+    resizing: true // Auto-resizing, read: https://github.com/VodkaBears/Vide#resizing
+  });
 
   // Review Slider
   $('.reviews').slick({
