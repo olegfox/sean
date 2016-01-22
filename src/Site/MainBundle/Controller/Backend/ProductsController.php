4739b95e7c6a3dc7f872f19373a78575174bd6f4
olegfox <info@olegfox.com>
2016-01-22 07:31:38 +0300

    lst version

--- src/Site/MainBundle/Controller/Backend/ProductsController.php
+++ src/Site/MainBundle/Controller/Backend/ProductsController.php
@@ -26,6 +26,7 @@ class ProductsController extends Controller
         $entities = $em->getRepository('SiteMainBundle:Products')->findAll();
 
         $paginator = $this->get('knp_paginator');
+
         $pagination = $paginator->paginate(
             $entities,
             $this->get('request')->query->get('page', 1) /*page number*/,
